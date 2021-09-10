06ef96617fd958b70a99f79dfd1bb101d23a4e47
MinhajulAkib <00akibswe@gmail.com>
2021-09-10 19:10:32 +0600

    types

--- output/script.js
+++ output/script.js
@@ -2,3 +2,7 @@
 console.log('hello');
 var country = 'i love bangladesh';
 console.log(country);
+var playerName = 'mashrafi';
+console.log(playerName);
+playerName = 34;
+console.log(playerName);
--- src/script.ts
+++ src/script.ts
@@ -2,3 +2,8 @@ console.log('hello');
 
 const country = 'i love bangladesh';
 console.log(country);
+
+let playerName = 'mashrafi';
+console.log(playerName);
+playerName = 34;
+console.log(playerName);
